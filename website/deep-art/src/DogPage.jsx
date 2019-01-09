77e4ee8410657b7a3be6b40448e497ffc9607005
Gillian Belton (Populus Group) <v-gibelt@microsoft.com>
2019-01-09 16:25:21 -0500

    cleaned up code

--- website/deep-art/src/DogPage.jsx
+++ website/deep-art/src/DogPage.jsx
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
-
 import { Button, Card, CardImg } from 'reactstrap';
-
-//import Card from '@material-ui/core/Card';
-//import CardContent from '@material-ui/core/CardContent';
-//import CardMedia from '@material-ui/core/CardMedia';
-//import Button from '@material-ui/core/Button';
-
 
 const ColumnsDiv = styled.div`
     display: flex;
--- website/deep-art/src/ExplorePage/GenArt.jsx
+++ website/deep-art/src/ExplorePage/GenArt.jsx
@@ -1,20 +1,7 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
-
 import { Box, Image} from 'grommet';
-
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-
-import Card from 'react-bootstrap/lib/Card';
 import vase from '../images/testVase.jpg';
-
-// const StyledCard = styled(Card)`
-//     &&{
-//         margin: 5vw;
-
-//     }`
 
 export default class GenArt extends Component {
     condtructor(props){
