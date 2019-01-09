5ff98c9f475b822f08e5b11161af7ffabee2f1fa
Gillian Belton (Populus Group) <v-gibelt@microsoft.com>
2019-01-09 16:15:38 -0500

    changes that will be discarded bc grommit

--- website/deep-art/src/DogPage.jsx
+++ website/deep-art/src/DogPage.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
 
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import { Button, Card, CardImg } from 'reactstrap';
+
+//import Card from '@material-ui/core/Card';
+//import CardContent from '@material-ui/core/CardContent';
+//import CardMedia from '@material-ui/core/CardMedia';
+//import Button from '@material-ui/core/Button';
 
 
 const ColumnsDiv = styled.div`
@@ -12,38 +14,59 @@ const ColumnsDiv = styled.div`
     flex-flow: row wrap;
 `
 const Http = new XMLHttpRequest();
-
 const url = 'https://dog.ceo/api/breeds/image/random';
-//'https://jsonplaceholder.typicode.com/posts'
-Http.open("GET", url);
-Http.send();
-Http.onreadystatechange = (e) => {
-
-    console.log(Http.responseText)
-}
+let imageUrl = "placeholder";
 
 export default class DogPage extends Component {
-    constructor(props){
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageUrl: "placeholder"
+        };
+        this.getDogPicture();
     };
 
     getDogPicture() {
-
+        Http.open("GET", url);
+        Http.send();
+        Http.onreadystatechange = (e) => {
+            try {
+                var imageURL = JSON.parse(Http.responseText).message;
+                this.setState({ imageUrl: imageURL });
+                this.render();
+            } catch (e) {
+                console.log('malformed request:' + Http.responseText);
+            }
+        }
     }
 
-    render(){
+    render() {
+        return (
+            
+            <div>
+                <Button color="primary">primary</Button>{' '}
+                <Button color="secondary">secondary</Button>{' '}
+                <Button color="success">success</Button>{' '}
+                <Button color="info">info</Button>{' '}
+                <Button color="warning">warning</Button>{' '}
+                <Button color="danger">danger</Button>{' '}
+                <Button color="link">link</Button>
+            </div>
+            
+            
+            );
         return(
             <ColumnsDiv>
                 <Card>
-                    <CardMedia
-                        image={require('./images/testVase.jpg')}
-                        title="dog?"
+                    <CardImg
+                        src={this.state.imageUrl}
+                        alt="dog?"
                         style={{minWidth: '100%', width: "30vw", paddingTop: '100%'}}
                     />
                 </Card>
-                <Button>
+                <Button color="primary">
                     SHOW ME DOG
-                </Button>
+                </Button>{' '}
             </ColumnsDiv>
         );
     }
