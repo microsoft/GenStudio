dd9bc002b44960dd74f670d0203d71102ab60d0a
Alonso Salas Infante (Populus Group) <v-alsal@microsoft.com>
2019-01-18 10:13:54 -0500

    Slightly broken Azure search code

--- website/deep-art/src/GraphPage/ImageGraph.jsx
+++ website/deep-art/src/GraphPage/ImageGraph.jsx
@@ -5,7 +5,7 @@ import landscape from '../images/testLandscape.jpg';
 
 let graph = {
     nodes: [
-        {id: 1, label: 'Node 1', shape: 'image', image:landscape},
+        {id: 1, label: 'Node 1', shape: 'image', image:"https://images.metmuseum.org/CRDImages/es/original/ES6063.jpg"},
         {id: 2, label: 'Node 2'},
         {id: 3, label: 'Node 3'},
         {id: 4, label: 'Node 4'},
@@ -58,7 +58,7 @@ export default class ImageGraph extends Component {
     render(){
         return(
             <Graph
-                graph={this.state.graph}
+                graph={{nodes: this.props.nodes, edges: this.props.edges}}
                 options={this.state.options.options}
                 events={this.state.events.events}
             />
