9521fb6badf8e87767b47ed4e65a72ef3b0261ad
Gillian Belton (Populus Group) <v-gibelt@microsoft.com>
2019-01-23 13:57:20 -0500

    updated to curated api and prelim expore page v2

--- API/app.py
+++ API/app.py
@@ -9,6 +9,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+curated_images = {'vessels' : [249414, 324431, 544501, 324830, 324917, 329070, 325676, 42447, 44072, 8288, 662161, 451766, 446996, 443137, 201671, 751402, 232038, 202194],
+'armors' : [22270, 24937, 25114, 22848, 35652, 22408, 23143],
+'jewelry' : [483898, 552392, 5405, 784629, 464073, 326937]
+}
+
 @app.route("/GetIDsByCategory")
 def handleRequest():
     category = str(request.args.get('category'))
@@ -20,17 +25,20 @@ def handleRequest():
 
 
 def getIDsbyCategory(category, numIDs, result_type):
-    tags = pd.read_csv('MetTaggingData12_7_2018.csv')
-    ids = tags[tags.Tags == category].ObjectID.values
     if result_type == "first":
-        return ids[:numIDs]
+        tags = pd.read_csv('MetTaggingData12_7_2018.csv')
+        ids = tags[tags.Tags == category].ObjectID.values
+        return ids[:numIDs].tolist()
     elif result_type == "curated":
-        pass
+        if len(curated_images[category]) < numIDs:
+            return curated_images[category]
+        return curated_images[category][:numIDs]
     else: #random
-        pass
+        raise ValueError('Random selection has not been implemented.')
+        
 
 def convertToJSON(npArr, category, num_ids, result_type):
-    response = {"ObjectIds": npArr.tolist()}
+    response = {"ObjectIds": npArr}
     response = json.dumps({'results': response})
     return response
 
