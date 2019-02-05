d016ff07b24164090b9748f35bf7ac8ca1e28f3d
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2019-02-05 16:21:03 +0100

    Finished Footer responsive styles

--- website/deep-art/src/Footer/Footer.jsx
+++ website/deep-art/src/Footer/Footer.jsx
@@ -1,47 +1,106 @@
 import React, { Component } from 'react';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import { Box, Button, Text } from 'grommet';
+import globe from '../images/globe.svg';
 
-const FootBox = styled(Box)`
-    padding-top: 2rem;
-    padding-bottom: 2rem;
-`
+const Footer = styled.footer`
+  background-color: #f2f2f2;
+  font-size: 12px;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  padding-right: 2.875rem;
+  padding-left: 2.875rem;
 
-const WrapBox = styled(Box)`
-    padding-left: 3rem;
-    padding-right: 3rem;
-`
+  @media (min-width: 768px) {
+
+  }
+`;
+
+const FooterDesc = styled.p`
+  margin-bottom: 1rem;
+  margin-top: 0;
+`;
+
+const LinkBig = styled.a`
+  color: #000000;
+  font-weight: 600;
+  margin-left: 0.25rem;
+  white-space: nowrap;
+`;
+
+const FooterLinks = styled.div`
+
+  @media (min-width: 48em) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Lang = styled.div`
+  align-items: center;
+  color: #505050;
+  display: flex;
+  margin-bottom: 0.5rem;
+  white-space: nowrap;
+
+  @media (min-width: 48em) {
+    margin-bottom: initial;
+  }
+`;
+
+const LangImg = styled.img`
+  margin-right: 0.5rem;
+  height: 1rem;
+  width: 1rem;
+`;
+
+const NoWrap = styled.span`
+  white-space: nowrap;
+`;
+
+const Link = styled.a`
+  display: block;
+  color: #505050;
+  margin-right: 1rem;
+  text-decoration: none;
+  white-space: nowrap;
+
+  @media (min-width: 33.75em) {
+    display: initial;
+  }
+`;
+
 /**
  * The Navigational Bar across the top of the page
  */
-export default class NavBar extends Component{
-
-    render() {
-        return(
-            <WrapBox>
-                <FootBox
-                    direction='column'
-                    align='center'
-                    justify='center'
-                    elevation='none'
-                    fill='horizontal'
-                    style={{alignContent: "space-between"}}
-                >
-                    <Text size="16px">
-                        {"Gen Studio is a prototype concept which was created over a two-day hackathon with collaborators across The Met, Microsoft, and MIT. "} 
-                        <Button hoverIndicator={true} href={"https://www.microsoft.com/inculture/arts/met-microsoft-mit-ai-open-access-hack"} style={{marginLeft: "5px"}}>
-                            <Text size="16px">
-                                Click here to learn more.
-                            </Text>
-                        </Button>
-                    </Text>
-
-
-                </FootBox>
-            </WrapBox>
-
-        );
-    }
+export default class NavBar extends Component {
+  render() {
+    return (
+      <Footer>
+        <FooterDesc>
+          Gen Studio is a prototype concept which was created over a two-day hackathon with
+          collaborators across The Met, Microsoft, and MIT.
+          <LinkBig hoverIndicator={true} href={'https://www.microsoft.com/inculture/arts/met-microsoft-mit-ai-open-access-hack'} target="_blank" rel="noopener">
+            Click here to learn more.
+          </LinkBig>
+        </FooterDesc>
+        <FooterLinks>
+          <Lang>
+            <LangImg src={globe} alt={"Language: English"} />
+            <NoWrap>
+              English (United States)
+            </NoWrap>
+          </Lang>
+          <div>
+            <Link href={'https://go.microsoft.com/fwlink/?LinkId=521839'} hoverIndicator={true} target="_blank" rel="noopener">Privacy &amp; Cookies</Link>
+            <Link href={'https://go.microsoft.com/fwlink/?LinkID=206977'} hoverIndicator={true} target="_blank" rel="noopener">Terms of use</Link>
+            <Link href={'https://www.microsoft.com/trademarks'} hoverIndicator={true} target="_blank" rel="noopener">Trademarks</Link>
+            <Link href={'https://support.microsoft.com/en-us/contactus'} hoverIndicator={true} target="_blank" rel="noopener">Report abuse</Link>
+            <NoWrap style={{whiteSpace: 'nowrap'}}>© Microsoft 2019</NoWrap>
+          </div>
+        </FooterLinks>
+      </Footer>
+    );
+  }
 }
