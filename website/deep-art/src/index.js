36679590227dca18e4315ef39f981ecfc77874c2
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2019-02-06 11:52:30 +0100

    Changed index.css to 'main.scss import

--- website/deep-art/src/index.js
+++ website/deep-art/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './main.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
