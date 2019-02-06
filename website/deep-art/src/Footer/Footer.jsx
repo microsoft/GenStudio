9e6a1643e6537b2c7741fbc86c31d0b70276e254
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2019-02-06 15:19:37 +0100

    Added translations to footer and nav

--- website/deep-art/src/Footer/Footer.jsx
+++ website/deep-art/src/Footer/Footer.jsx
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
 
+import { withNamespaces } from 'react-i18next';
+
 import globe from '../images/globe.svg';
 
 /**
  * The Navigational Bar across the top of the page
  */
-export default class NavBar extends Component {
-  render() {
+function NavBar({ t }) {
     return (
       <footer className="foo">
         <p className="foo__desc">
-          Gen Studio is a prototype concept which was created over a two-day hackathon with
-          collaborators across The Met, Microsoft, and MIT.
+          {t('foo.desc')}
           <a className="foo__link foo__link--strong" hoverIndicator={true} href={'https://www.microsoft.com/inculture/arts/met-microsoft-mit-ai-open-access-hack'} target="_blank" rel="noopener noreferrer">
-            Click here to learn more.
+            {t('global.learn_more')}
           </a>
         </p>
         <div className="foo__links">
           <div className="foo__lang">
-            <img className="foo__img" src={globe} alt={"Language: English"} />
-            <span className="u-nowrap">English (United States)</span>
+            <img className="foo__img" src={globe} alt={t('global.lang_eng')} />
+            <span className="u-nowrap">{t('global.lang_eng_description_us')}</span>
           </div>
           <div>
-            <a className="foo__link" href="https://go.microsoft.com/fwlink/?LinkId=521839" target="_blank" rel="noopener noreferrer">Privacy &amp; Cookies</a>
-            <a className="foo__link" href="https://go.microsoft.com/fwlink/?LinkID=206977" target="_blank" rel="noopener noreferrer">Terms of use</a>
-            <a className="foo__link" href="https://www.microsoft.com/trademarks" target="_blank" rel="noopener noreferrer">Trademarks</a>
-            <a className="foo__link" href="https://support.microsoft.com/en-us/contactus" target="_blank" rel="noopener noreferrer">Report abuse</a>
-            <span className="u-nowrap">© Microsoft 2019</span>
+            <a className="foo__link" href="https://go.microsoft.com/fwlink/?LinkId=521839" target="_blank" rel="noopener noreferrer">{t('foo.privacy_cookies')}</a>
+            <a className="foo__link" href="https://go.microsoft.com/fwlink/?LinkID=206977" target="_blank" rel="noopener noreferrer">{t('foo.terms_use')}</a>
+            <a className="foo__link" href="https://www.microsoft.com/trademarks" target="_blank" rel="noopener noreferrer">{t('foo.trademarks')}</a>
+            <a className="foo__link" href="https://support.microsoft.com/en-us/contactus" target="_blank" rel="noopener noreferrer">{t('foo.report_abuse')}</a>
+            <span className="u-nowrap">{t('global.copyright')}</span>
           </div>
         </div>
       </footer>
-    );
-  }
+  );
 }
+
+export default withNamespaces()(NavBar);
--- website/deep-art/src/NavBar/NavBar.jsx
+++ website/deep-art/src/NavBar/NavBar.jsx
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 
+import { withNamespaces } from 'react-i18next';
+
 import search from './../images/search.svg';
 
 /**
  * The Navigational Bar across the top of the page
  */
-export default class NavBar extends Component {
-  render() {
-    return (
-      <nav className="nav">
-        <a className="nav__link" href="/">Gen Studio</a>
-        <a className="nav__btn" href="/search/1">
-          <span className="nav__text">Explore the MET Collection</span>
-          <img className="nav__img" src={search} alt={'Explore the MET Collection'} />
-        </a>
-      </nav>
-    );
-  }
+function NavBar({ t }) {
+  return (
+    <nav className="nav">
+      <a className="nav__link" href="/">
+        {t('global.title')}
+      </a>
+      <a className="nav__btn" href="/search/1">
+        <span className="nav__text">{t('nav.explore')}</span>
+        <img className="nav__img" src={search} alt={t('nav.explore')} />
+      </a>
+    </nav>
+  );
 }
+
+export default withNamespaces()(NavBar);
