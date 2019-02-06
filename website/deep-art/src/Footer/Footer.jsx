6f6b9ffe83b9eb6eab457a4a7bc867ef5ab9e029
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2019-02-06 13:07:42 +0100

    Updated footer HTML

--- website/deep-art/src/Footer/Footer.jsx
+++ website/deep-art/src/Footer/Footer.jsx
@@ -12,7 +12,7 @@ export default class NavBar extends Component {
         <p className="foo__desc">
           Gen Studio is a prototype concept which was created over a two-day hackathon with
           collaborators across The Met, Microsoft, and MIT.
-          <a className="foo__link foo__link--strong" hoverIndicator={true} href={'https://www.microsoft.com/inculture/arts/met-microsoft-mit-ai-open-access-hack'} target="_blank" rel="noopener">
+          <a className="foo__link foo__link--strong" hoverIndicator={true} href={'https://www.microsoft.com/inculture/arts/met-microsoft-mit-ai-open-access-hack'} target="_blank" rel="noopener noreferrer">
             Click here to learn more.
           </a>
         </p>
@@ -22,10 +22,10 @@ export default class NavBar extends Component {
             <span className="u-nowrap">English (United States)</span>
           </div>
           <div>
-            <a className="foo__link" href="https://go.microsoft.com/fwlink/?LinkId=521839" target="_blank" rel="noopener">Privacy &amp; Cookies</a>
-            <a className="foo__link" href="https://go.microsoft.com/fwlink/?LinkID=206977" target="_blank" rel="noopener">Terms of use</a>
-            <a className="foo__link" href="https://www.microsoft.com/trademarks" target="_blank" rel="noopener">Trademarks</a>
-            <a className="foo__link" href="https://support.microsoft.com/en-us/contactus" target="_blank" rel="noopener">Report abuse</a>
+            <a className="foo__link" href="https://go.microsoft.com/fwlink/?LinkId=521839" target="_blank" rel="noopener noreferrer">Privacy &amp; Cookies</a>
+            <a className="foo__link" href="https://go.microsoft.com/fwlink/?LinkID=206977" target="_blank" rel="noopener noreferrer">Terms of use</a>
+            <a className="foo__link" href="https://www.microsoft.com/trademarks" target="_blank" rel="noopener noreferrer">Trademarks</a>
+            <a className="foo__link" href="https://support.microsoft.com/en-us/contactus" target="_blank" rel="noopener noreferrer">Report abuse</a>
             <span className="u-nowrap">© Microsoft 2019</span>
           </div>
         </div>
