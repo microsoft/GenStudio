f27e39e356359b12e9a1df3a2381152e0fef7826
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2019-02-06 12:19:40 +0100

    Moved styled components to Sass

--- website/deep-art/src/Footer/Footer.jsx
+++ website/deep-art/src/Footer/Footer.jsx
@@ -1,76 +1,6 @@
 import React, { Component } from 'react';
 
-import styled from 'styled-components';
-
 import globe from '../images/globe.svg';
-
-const Footer = styled.footer`
-  background-color: #f2f2f2;
-  font-size: 12px;
-  margin-left: -6.25rem;
-  margin-right: -6.25rem;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  padding-right: 2.875rem;
-  padding-left: 2.875rem;
-
-  @media (min-width: 768px) {
-
-  }
-`;
-
-const FooterDesc = styled.p`
-  margin-bottom: 1rem;
-  margin-top: 0;
-`;
-
-const LinkBig = styled.a`
-  color: #000000;
-  font-weight: 600;
-  margin-left: 0.25rem;
-  white-space: nowrap;
-`;
-
-const FooterLinks = styled.div`
-  @media (min-width: 48em) {
-    display: flex;
-    justify-content: space-between;
-  }
-`;
-
-const Lang = styled.div`
-  align-items: center;
-  color: #505050;
-  display: flex;
-  margin-bottom: 0.5rem;
-  white-space: nowrap;
-
-  @media (min-width: 48em) {
-    margin-bottom: initial;
-  }
-`;
-
-const LangImg = styled.img`
-  margin-right: 0.5rem;
-  height: 1rem;
-  width: 1rem;
-`;
-
-const NoWrap = styled.span`
-  white-space: nowrap;
-`;
-
-const Link = styled.a`
-  display: block;
-  color: #505050;
-  margin-right: 1rem;
-  text-decoration: none;
-  white-space: nowrap;
-
-  @media (min-width: 33.75em) {
-    display: initial;
-  }
-`;
 
 /**
  * The Navigational Bar across the top of the page
@@ -78,30 +8,28 @@ const Link = styled.a`
 export default class NavBar extends Component {
   render() {
     return (
-      <Footer>
-        <FooterDesc>
+      <footer className="foo">
+        <p className="foo__desc">
           Gen Studio is a prototype concept which was created over a two-day hackathon with
           collaborators across The Met, Microsoft, and MIT.
-          <LinkBig hoverIndicator={true} href={'https://www.microsoft.com/inculture/arts/met-microsoft-mit-ai-open-access-hack'} target="_blank" rel="noopener">
+          <a className="foo__link foo__link--strong" hoverIndicator={true} href={'https://www.microsoft.com/inculture/arts/met-microsoft-mit-ai-open-access-hack'} target="_blank" rel="noopener">
             Click here to learn more.
-          </LinkBig>
-        </FooterDesc>
-        <FooterLinks>
-          <Lang>
-            <LangImg src={globe} alt={"Language: English"} />
-            <NoWrap>
-              English (United States)
-            </NoWrap>
-          </Lang>
-          <div>
-            <Link href={'https://go.microsoft.com/fwlink/?LinkId=521839'} hoverIndicator={true} target="_blank" rel="noopener">Privacy &amp; Cookies</Link>
-            <Link href={'https://go.microsoft.com/fwlink/?LinkID=206977'} hoverIndicator={true} target="_blank" rel="noopener">Terms of use</Link>
-            <Link href={'https://www.microsoft.com/trademarks'} hoverIndicator={true} target="_blank" rel="noopener">Trademarks</Link>
-            <Link href={'https://support.microsoft.com/en-us/contactus'} hoverIndicator={true} target="_blank" rel="noopener">Report abuse</Link>
-            <NoWrap style={{whiteSpace: 'nowrap'}}>© Microsoft 2019</NoWrap>
+          </a>
+        </p>
+        <div className="foo__links">
+          <div className="foo__lang">
+            <img className="foo__img" src={globe} alt={"Language: English"} />
+            <span className="u-nowrap">English (United States)</span>
           </div>
-        </FooterLinks>
-      </Footer>
+          <div>
+            <a className="foo__link" href="https://go.microsoft.com/fwlink/?LinkId=521839" target="_blank" rel="noopener">Privacy &amp; Cookies</a>
+            <a className="foo__link" href="https://go.microsoft.com/fwlink/?LinkID=206977" target="_blank" rel="noopener">Terms of use</a>
+            <a className="foo__link" href="https://www.microsoft.com/trademarks" target="_blank" rel="noopener">Trademarks</a>
+            <a className="foo__link" href="https://support.microsoft.com/en-us/contactus" target="_blank" rel="noopener">Report abuse</a>
+            <span className="u-nowrap">© Microsoft 2019</span>
+          </div>
+        </div>
+      </footer>
     );
   }
 }
--- website/deep-art/src/NavBar/NavBar.jsx
+++ website/deep-art/src/NavBar/NavBar.jsx
@@ -1,46 +1,6 @@
 import React, { Component } from 'react';
 
-import styled from 'styled-components';
-
 import search from './../images/search.svg';
-
-const Nav = styled.nav`
-  align-items: center;
-  display: flex;
-  height: 3.125rem;
-  justify-content: space-between;
-  margin-bottom: 1.25rem;
-`;
-
-const Link = styled.a`
-  color: #000000;
-  font-size: 1.25rem;
-`;
-
-const Search = styled.a`
-  align-items: center;
-  border: 1px solid #d2d2d2;
-  border-radius: 1.5rem;
-  color: #505050;
-  display: flex;
-  height: 2.125rem;
-  justify-content: space-between;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  text-decoration: none;
-  width: 15rem;
-`;
-
-const SearchIcon = styled.img`
-  margin-left: 0.5rem;
-  height: 1.25rem;
-  width: 1.25rem;
-`;
-
-const SearchDesc = styled.span`
-  font-size: 0.75rem;
-  white-space: nowrap;
-`;
 
 /**
  * The Navigational Bar across the top of the page
@@ -48,13 +8,13 @@ const SearchDesc = styled.span`
 export default class NavBar extends Component {
   render() {
     return (
-      <Nav>
-        <Link hoverIndicator="false" style={{ textDecoration: 'none' }} href={'/'}>Gen Studio</Link>
-        <Search href="/search/1">
-          <SearchDesc>Explore the MET Collection</SearchDesc>
-          <SearchIcon src={search} alt={'Explore the MET Collection'} />
-        </Search>
-      </Nav>
+      <nav className="nav">
+        <a className="nav__link" href="/">Gen Studio</a>
+        <a className="nav__btn" href="/search/1">
+          <span className="nav__text">Explore the MET Collection</span>
+          <img className="nav__img" src={search} alt={'Explore the MET Collection'} />
+        </a>
+      </nav>
     );
   }
 }
