22c2a110bc61fe17053e8c4d5e66aae830347a2b
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2019-02-06 15:19:52 +0100

    Added translation and logo styles in Sass

--- website/deep-art/src/Logo/Logo.jsx
+++ website/deep-art/src/Logo/Logo.jsx
@@ -1,21 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import styled from 'styled-components';
+import { withNamespaces } from 'react-i18next';
 
-const LogoTitle = styled.h1`
-  color: #000000;
-  font-size: 46px;
-  font-weight: 300;
-  line-height: initial;
-  margin-bottom: 1rem;
-  margin-top: 0;
-`;
-
-/**
- * Logo component of the page
- */
-export default class LogoText extends Component {
-  render() {
-    return <LogoTitle>Gen Studio</LogoTitle>;
-  }
+function LogoText({ t }) {
+  return <h1 className="logo">{t("global.title")}</h1>;
 }
+
+export default withNamespaces()(LogoText);
