79c868a490d6db34d2bd4f20181ddc026834038d
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2019-02-07 17:36:54 +0100

    Removed search button in NavBar

--- website/deep-art/src/NavBar/NavBar.jsx
+++ website/deep-art/src/NavBar/NavBar.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { withNamespaces } from 'react-i18next';
 
-import search from './../images/search.svg';
-
 /**
  * The Navigational Bar across the top of the page
  */
@@ -13,11 +11,6 @@ function NavBar({ t }) {
       <a className="nav__link" href="/">
         <span>{t('global.title')}</span>
       </a>
-      {/* TODO: Hidden search bar
-       <a className="nav__button" href="/search/1">
-        <span className="nav__text">{t('nav.explore')}</span>
-        <img className="nav__image" src={search} alt={t('nav.explore')} />
-      </a> */}
     </nav>
   );
 }
