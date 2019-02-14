f4a6d808b3f611cb6e8ce0cd8759fa1d9af6dc6c
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2019-02-14 14:55:35 +0100

    Cleaned ExplorePAlette HTML

--- website/deep-art/src/ExplorePage/ExplorePalette.jsx
+++ website/deep-art/src/ExplorePage/ExplorePalette.jsx
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import PaletteBox from './PaletteBox.jsx';
 
-import { Grid } from 'grommet';
-
 /**
  * The Palette used to explore the images
  * 'addSeed' prop: callback to add an image to the current seed
  * 'subSeed' prop: callback to subtract an image from the current seed
  */
 export default class ExplorePalette extends Component {
-
-    render(){
-        return(
-            <Grid
-                columns={["small", "small","small"]}
-                rows={"meduim"}
-                gap="medium"
-                margin="40px"
-            >
-                {this.props.images.map(tile => (
-                    <PaletteBox key={tile.key} img={tile.img} id={tile.id} addSeed={this.props.addSeed} subSeed={this.props.subSeed}/>
-                ))}
-            </Grid>
-        );
-    }
+  render() {
+    return (
+      <div className="explore__grid">
+        {this.props.images.map(tile => (
+          <PaletteBox
+            key={tile.key}
+            img={tile.img}
+            id={tile.id}
+            addSeed={this.props.addSeed}
+            subSeed={this.props.subSeed}
+          />
+        ))}
+      </div>
+    );
+  }
 }
