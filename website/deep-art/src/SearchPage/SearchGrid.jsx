f25e7abbd7db3c6488962e8790d3726d2fd35e53
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2019-02-15 13:34:48 +0100

    Cleaned SearchGrid gormet component and added proper HTML

--- website/deep-art/src/SearchPage/SearchGrid.jsx
+++ website/deep-art/src/SearchPage/SearchGrid.jsx
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
 import ResultBox from './ResultBox.jsx';
 
-import { Grid } from 'grommet';
-
 /**
  * Grid used to display results of a search
  * 'results' prop: an array of the json results from the Azure search (the 'value' value)
  */
 export default class SearchGrid extends Component {
-
-    render(){
-        return(
-            <Grid
-                columns={"300px"}
-                rows={"flex"}
-                gap="small"
-                margin="40px"
-            >
-                {this.props.results.map(result => (
-                    <ResultBox key={result.ObjectID} data={result} />
-                ))}
-            </Grid>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.results.map(result => (
+          <ResultBox key={result.ObjectID} data={result} />
+        ))}
+      </React.Fragment>
+    );
+  }
 }
