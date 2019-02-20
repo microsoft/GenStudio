0aafe19827bebe576195ce8a41e637fcfb9e575d
PLAINCONCEPTS\fpelaez <fpelaez@plainconcepts.com>
2019-02-20 13:40:09 -0800

    get the title from the home page back

--- website/deep-art/src/SelectPage/SelectPage.jsx
+++ website/deep-art/src/SelectPage/SelectPage.jsx
@@ -245,7 +245,7 @@ class SelectPage extends Component {
         {t => (
           <React.Fragment>
             <div className="selectpage__head">
-              {/* <h1 className="claim">{t('global.claim')}</h1> */}
+              <h1 className="claim">{t('global.claim')}</h1>
               <SelectControl
                 sendObjectIds={this.getImageIDs}
                 clearOldImages={this.clearOldImages}
