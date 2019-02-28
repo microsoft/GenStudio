6b3f5ad0bd05b957c0e8845c3157cce1d6eae47a
marhamil723 <mhamilton723@gmail.com>
2019-02-27 19:47:33 -0800

    add app insights

--- website/src/index.js
+++ website/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import ReactAI from 'react-appinsights';
+ReactAI.init({instrumentationKey:'4f750fc5-13d7-4145-84a6-a528aec1f9f6'});
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
